8700f69ab747df4e7beb05666da4b5f2e1137397
Tejas12malviya <91682736+Tejas12malviya@users.noreply.github.com>
2024-12-04 19:57:56 +0530

    Add files via upload

---
+++ nested_models.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+from pydantic import BaseModel, HttpUrl
+from typing import List
+
+app=FastAPI()
+
+class Image(BaseModel):
+    url:HttpUrl
+    name :str
+
+class Item(BaseModel):
+    name:str
+    Description:str |None=None
+    price:float
+    tax:float |None=None
+    tags:set[str]=set()
+    Images:list[Image] | None=None
+
+@app.put("/item/{item_id}")
+def update_item(item_id:int,item:Item):
+    results={"item_id":item_id,"item":item}
+    return results
+ 
+class Image(BaseModel):
+    url: HttpUrl
+    name: str
+
+
+@app.post("/images/multiple/")
+async def create_multiple_images(images: list[Image]):
+    return images 
