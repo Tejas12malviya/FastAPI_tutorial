796b2fdb09834aae029f4b416205bcbf219366cd
Tejas12malviya <91682736+Tejas12malviya@users.noreply.github.com>
2024-12-04 20:29:57 +0530

    Update nested_models.py

--- nested_models.py
+++ nested_models.py
@@ -20,12 +20,50 @@ class Item(BaseModel):
 def update_item(item_id:int,item:Item):
     results={"item_id":item_id,"item":item}
     return results
- 
+
+
+
+
+
+
+
 class Image(BaseModel):
     url: HttpUrl
     name: str
 
-
 @app.post("/images/multiple/")
 async def create_multiple_images(images: list[Image]):
     return images 
+
+
+
+
+
+
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+@app.put("/items/{item_id}")
+def update_item(
+    item_id:int,
+    item:Annotated[
+        Item,
+        Body(     # contains predefined values as given in examples
+            examples=[
+                {
+                    "name":"Foo",
+                    "description":"A very nice Item",
+                    "price":25.1,
+                    "tax":2.1
+                }
+            ]
+        )
+    ]
+
+):
+    result={"item_id":item_id,"item":item}
+    return result
