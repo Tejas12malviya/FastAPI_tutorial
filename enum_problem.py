c96cc56afe8bbf09a3e1db37a258e361dd2f1152
Tejas12malviya <91682736+Tejas12malviya@users.noreply.github.com>
2024-12-05 09:53:07 +0530

    Update enum_problem.py

--- enum_problem.py
+++ enum_problem.py
@@ -21,11 +21,16 @@ def get_model(model_name: ModelName):
     return {"model_name": model_name,"message":"Have some residuals"}
 
 
+# ----------------------------------------------------------------------------------------------------------
+
+# int this query parameter if we put skip=1 and limit=1, then from the given list of  # Deprecated means the given function is still available but is no longer recommended for use we get str of "item_name":"Bar" because it skips the first on and limit commands to to print inly one.
 fake_items_db = [{"item_name":"Foo"},{"item_name":"Bar"},{"item_name":"Baz"}]
 
 @app.get("/items/")
 def read_item(skip:int=0, limit:int=10):
     return fake_items_db[skip: skip+limit] 
+
+# ---------------------------------------------------------------------------------------------------------- 
 
 @app.get("/users/{user_id}/items/{item_id}")
 def read_user_items(
